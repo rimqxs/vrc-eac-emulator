524c9d4e9b0a78700e8b954494176eb74b60b63c
unknown <kamiskidder@outlook.jp>
2025-03-08 00:43:56 +0900

    Fix data mismatch of incoming data

--- src/linux/servers/socket_server.cpp
+++ src/linux/servers/socket_server.cpp
@@ -1,12 +1,12 @@
 #include "socket_server.h"
 
-#include <future>
-#include <plog/Log.h>
-
-#include <common/socket.h>
 #include <common/constants.h>
 #include <common/protocol/packet_codec.h>
+#include <common/socket.h>
 #include <httplib.h>
+#include <plog/Log.h>
+
+#include <future>
 
 #include "../handlers/handler_registry.h"
 
@@ -19,14 +19,12 @@ void socket_server::launch() {
 		PLOGE.printf("socks::init failed");
 		return;
 	}
-
 	SOCKET socket_;
 	addrinfo* info;
 	if (socket::listen(HOST_SOCKET_PORT, &socket_, &info) != NULL) {
 		PLOGE.printf("Server setup failed");
 		return;
 	}
-
 	PLOGI.printf("Server started on localhost:%d! Waiting for connection...", HOST_SOCKET_PORT);
 	if (socket::accept(socket_, &clientSocket) != NULL) {
 		PLOGE.printf("Accepting client connection failed");
@@ -46,18 +44,16 @@ void socket_server::send_packet(const std::shared_ptr<packet>& packet) {
 
 void socket_server::receive_loopback() {
 	bool running = true;
-	char buf[DEFAULT_BUFLEN];
-	int buf_len = DEFAULT_BUFLEN;
 	while (running) {
-		int received_len = recv(clientSocket, buf, buf_len, 0);
-		if (received_len <= 0) {
+		std::vector<char> received_buffer = socket::recv(clientSocket);
+		if (received_buffer.size() <= 0) {
 			PLOGW.printf("Connection closed");
 			running = false;
 			continue;
 		}
 
 		receive_mutex.lock();
-		read_stream stream(buf, received_len);
+		read_stream stream(received_buffer.data(), received_buffer.size());
 		while (stream.bytes_remaining() > 0) {
 			auto packet = packet_codec::decode(stream);
 			if (packet == nullptr) {
@@ -73,18 +69,22 @@ void socket_server::receive_loopback() {
 }
 
 void socket_server::tick() {
-	// perform receiving
+	performReceive();
+	performSend();
+}
+
+void socket_server::performSend() {
 	receive_mutex.lock();
 	for (const auto& packet : receive_queued_packets) {
-		if (const auto handler
-			= handler_registry::get_handler_by_id(packet->get_id())) {
+		if (const auto handler = handler_registry::get_handler_by_id(packet->get_id())) {
 			handler(packet);
 		}
 	}
 	receive_queued_packets.clear();
 	receive_mutex.unlock();
+}
 
-	// perform sending
+void socket_server::performReceive() {
 	send_mutex.lock();
 	for (auto& packet : send_queued_packets) {
 		write_stream stream = packet_codec::encode(packet);
